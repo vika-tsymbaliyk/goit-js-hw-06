6df6a7b12ac15a4f4be293c8c8e167e2b21da462
vika-tsymbaliyk <vika.bronytska@gmail.com>
2023-06-15 00:39:38 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -10,16 +10,16 @@ const buttonCreate = document.querySelector('button[data-create]');
 const buttonDestroy = document.querySelector('button[data-destroy]');
 const createArea = document.querySelector('#boxes');
 
-// function createBoxes() {
-//   const amount = controlInput.value;
-//   console.log(amount);
-//   let i = 0;
-//   while (i <= amount) {
+function createBoxes() {
+  const amount = controlInput.value;
+  console.log(amount);
+ 
+  for (let i = 0; i < amount; i += 1){
 //     const newDiv = document.createElement('div');
 //     newDiv.style.height = "30px";
 //     newDiv.style.width = "30px";
 //     newDiv.style.backgroundColor = getRandomHexColor();
-//   }
-// }
-// buttonCreate.addEventListener("click", createBoxes);
+ }
+}
+buttonCreate.addEventListener("click", createBoxes);
 
