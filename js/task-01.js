c11b1b6815b15ec10139ff62d8b97bed46319fc3
vika-tsymbaliyk <vika.bronytska@gmail.com>
2023-06-17 14:19:35 +0300

    new

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,8 @@
-const categoriesList = document.querySelector('#categories');
 const categoriesItems = categoriesList.querySelectorAll('.item');
 console.log(`Number of categories: ${categoriesItems.length}`);
 
 categoriesItems.forEach(element => {
-    console.log(`Category: ${element.children[0].textContent}`);
-    console.log(`Elements: ${element.children[1].children.length}`)
+    console.log(`Category: ${element.firstElementChild.textContent}`);
+    console.log(`Elements: ${element.lastElementChild.children.length}`)
     
 });
--- js/task-06.js
+++ js/task-06.js
@@ -1,10 +1,12 @@
 const inputForm = document.querySelector('#validation-input');
+const MIN_VALUE = parseInt(inputForm.getAttribute('data-length'));
+
+
 inputForm.addEventListener("blur", (event) => {
-    const inputText = event.currentTarget.value;
-    const MIN_VALUE = inputForm.getAttribute('data-length');
-    if (inputText.length == MIN_VALUE) {
-        inputForm.classList.add('valid');
-    } else {
-        inputForm.classList.add('invalid');
-    }
+    const inputText = inputForm.value.trim();
+    inputForm.classList.add('invalid');
+    if (inputText.length === MIN_VALUE) {
+        inputForm.classList.replace('invalid','valid');   
+    };    
 })
+
